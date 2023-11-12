6ad57f06ddfd33edd70cddfb748faafa43e2204b
Parksangwoo <psu8430@gmail.com>
2023-11-13 02:24:08 +0900

    test: 사용자 메뉴 및 수량 입력값 검증 테스트 코드 추가

--- __tests__/VarificatorManagerTest.js
+++ __tests__/VarificatorManagerTest.js
@@ -24,4 +24,27 @@ describe('VarificatorManager 테스트', () => {
 			expect(() => VarificatorManager.checkVisitDate(date)).toThrow(ERROR_MESSAGE.prefix);
 		});
 	});
+
+	describe('checkOrder => 사용자가 입력한 주문 메뉴와 메뉴의 수를 검증합니다.', () => {
+		test.each([
+			['티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1'],
+			['해산물파스타-2,레드와인-1,초코케이크-1'],
+			['샴페인-1'],
+		])('양식에 맞는 주문( "%s" )이 입력 되었을때 에러를 표시하지 않습니다.', (order) => {
+			//when, then
+			expect(() => VarificatorManager.checkOrder(order)).not.toThrow();
+		});
+
+		test.each([
+			['해산물파스타-2,레드와인-1,초코케이크-0'],
+			['해산물파스타-2,레드와인-1,초코케이크-'],
+			['해산물파스타-2,레드와인-abc,초코케이크-1'],
+			['해산물파스타-2,레드와인-1,고추바사삭-1'],
+			['해산물파스타=2,레드와인=1,초코케이크=1'],
+			['해산물파스타2,레드와인1,초코케이크1'],
+		])('양식에 맞지 않는 주문( "%s" )이 입력 되었을때 에러를 표시합니다.', (order) => {
+			//when, then
+			expect(() => VarificatorManager.checkOrder(order)).toThrow(ERROR_MESSAGE.prefix);
+		});
+	});
 });
--- __tests__/VarificatorTest.js
+++ __tests__/VarificatorTest.js
@@ -25,7 +25,7 @@ describe('Varificator', () => {
 		});
 	});
 
-	describe('isNotNumberInRange => 숫자가 범위 내에 있는 숫자인지 판다한다.', () => {
+	describe('isNotNumberInRange => 숫자가 범위 내에 있는 숫자인지 판단한다.', () => {
 		test.each([
 			// given
 			[8],
@@ -48,6 +48,162 @@ describe('Varificator', () => {
 			const minRange = 5;
 
 			expect(Varificator.isNotNumberInRange(value, maxRange, minRange)).toBe(true);
+		});
+	});
+
+	describe('isInvalidOrderformat => 주문이 주문 양식에 맞는지 판단한다.', () => {
+		// given
+		test.each([
+			['해산물파스타-2,레드와인-11,초코케이크-1'],
+			['고추바사삭-2'],
+			['포테이토피자-2,레드와인-1,사이다-2,치즈스파게티-1'],
+		])('%s는 주문 양식과 일치한다.', (order) => {
+			// when, then
+			expect(Varificator.isInvalidOrderformat(order)).toBe(false);
+		});
+
+		test.each([
+			// given
+			['해산물파스타-2,레드와인-11,초코케이크-'],
+			['고추바사삭-2,'],
+			['포테이토피자-2,레'],
+			['포테이토피자-ㅇㄹㄴ,'],
+		])('%s는 주문 양식과 일치하지 않는다.', (order) => {
+			// when, then
+			expect(Varificator.isInvalidOrderformat(order)).toBe(true);
+		});
+	});
+
+	describe('isMenuNotInMenuBoard => 메뉴판에 있는 메뉴인지 판단한다.', () => {
+		test.each([
+			// given
+			['제로콜라-1', [['제로콜라', '1']]],
+			[
+				'레드와인-1,시저샐러드-1',
+				[
+					['레드와인', '1'],
+					['시저샐러드', '1'],
+				],
+			],
+			[
+				'샴페인-1,타파스-1,해산물파스타-1',
+				[
+					['샴페인', '1'],
+					['타파스', '1'],
+					['해산물파스타', '1'],
+				],
+			],
+		])('주문 "%s"는 메뉴판에 있는 메뉴만 주문했다.', (_, order) => {
+			// when, then
+			expect(Varificator.isMenusNotInMenuBoard(order)).toBe(false);
+		});
+
+		test.each([
+			// given
+			['사이다-1', [['사이다', '1']]],
+			[
+				'레드와인-1,고추바사삭-1',
+				[
+					['레드와인', '1'],
+					['고추바사삭', '1'],
+				],
+			],
+			[
+				'샴페인-1,타파스-1,해산물파스타-1',
+				[
+					['샴페인', '1'],
+					['치킨무', '1'],
+					['해산물파스타', '1'],
+				],
+			],
+		])('주문 "%s"는 메뉴판에 없는 메뉴를 포함하고 있다.', (_, order) => {
+			// when, then
+			expect(Varificator.isMenusNotInMenuBoard(order)).toBe(true);
+		});
+	});
+
+	describe('isInvalidMenuCount => 주문한 메뉴의 개수가 유효한 개수인지 판단한다.', () => {
+		test.each([
+			[
+				'레드와인-1,시저샐러드-11',
+				[
+					['레드와인', '1'],
+					['시저샐러드', '11'],
+				],
+			],
+			[
+				'샴페인-12,타파스-1,해산물파스타-1323',
+				[
+					['샴페인', '12'],
+					['타파스', '1'],
+					['해산물파스타', '1323'],
+				],
+			],
+		])('주문 "%s"는 메뉴중 메뉴의 개수가 유효하다..', (_, order) => {
+			expect(Varificator.isInvalidMenuCount(order)).toBe(false);
+		});
+
+		test.each([
+			['제로콜라-ㅁ', [['제로콜라', 'ㅁ']]],
+			[
+				'레드와인-1,시저샐러드-1.1',
+				[
+					['레드와인', '1'],
+					['시저샐러드', '1.1'],
+				],
+			],
+			[
+				'샴페인-0,타파스-1,해산물파스타-1323',
+				[
+					['샴페인', '0'],
+					['타파스', '1'],
+					['해산물파스타', '1323'],
+				],
+			],
+		])('주문 "%s"는 메뉴중 메뉴의 개수가 유효하지 않다.', (_, order) => {
+			expect(Varificator.isInvalidMenuCount(order)).toBe(true);
+		});
+	});
+
+	describe('isDuplicatedMenu => 주문 중 중복된 메뉴가 있는지 판단한다.', () => {
+		test.each([
+			[
+				'레드와인-1,시저샐러드-1',
+				[
+					['레드와인', '1'],
+					['시저샐러드', '1'],
+				],
+			],
+			[
+				'샴페인-1,타파스-1,해산물파스타-1323',
+				[
+					['샴페인', '1'],
+					['타파스', '1'],
+					['해산물파스타', '2'],
+				],
+			],
+		])('주문 "%s"는 메뉴중 중복된 메뉴가 없다.', (_, order) => {
+			expect(Varificator.isDuplicatedMenu(order)).toBe(false);
+		});
+
+		test.each([
+			[
+				'레드와인-1,레드와인-4',
+				[
+					['레드와인', '1'],
+					['레드와인', '4'],
+				],
+			],
+			[
+				'해산물파스타-1,타파스-1,해산물파스타-1323',
+				[
+					['해산물파스타', '1'],
+					['타파스', '1'],
+					['해산물파스타', '1323'],
+				],
+			],
+		])('주문 "%s"는 메뉴중 메뉴의 개수가 유효하지 않다.', (_, order) => {
+			expect(Varificator.isDuplicatedMenu(order)).toBe(true);
 		});
 	});
 });
