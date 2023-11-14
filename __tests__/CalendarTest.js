036635f82cfce7fa06012dad93d11633e3d09db1
Parksangwoo <psu8430@gmail.com>
2023-11-14 15:00:40 +0900

    test: Calendar 클래스 관련 테스트 코드 추가

---
+++ __tests__/CalendarTest.js
@@ -0,0 +1,28 @@
+import Calendar from '../src/Model/Calendar';
+
+describe('Calendar', () => {
+	test('캘린더 인스턴스는 방문 일자 값을 가지고 있다.', () => {
+		// given
+		const visitDate = 10;
+
+		// when
+		const calendar = new Calendar(visitDate);
+
+		// then
+		expect(calendar.visitDate).toBe(10);
+	});
+
+	test('캘린더 인스턴스는 방문일에 받을 수 있는 혜택 여부를 알 수 있다.', () => {
+		// given
+		const visitDate = 25;
+		// when
+		const calendar = new Calendar(visitDate);
+
+		// then
+		expect(calendar.dayBenefit).toEqual({
+			isCristmasBenefit: true,
+			isWeedayBenefit: true,
+			isSpecialBenefit: true,
+		});
+	});
+});
