c160103aa95a49b7e26cb1832293b2f42bd58b63
Parksangwoo <psu8430@gmail.com>
2023-11-14 15:18:37 +0900

    feat: Order 클래스 추가

--- src/Model/Order.js
+++ src/Model/Order.js
@@ -1,9 +1,52 @@
 class Order {
+	#order;
+
+	#menuBoard;
+
+	constructor(order, menuBoard) {
+		const orders = Order.orderFormatter(order);
+		this.#menuBoard = menuBoard;
+		this.#order = Order.#getOrderDataWithPrice(orders, menuBoard);
+	}
+
 	static orderFormatter(order) {
 		const splittedOrders = order.split(',');
 		const formattedOrder = splittedOrders.map((splittedOrder) => splittedOrder.split('-'));
 
 		return formattedOrder;
+	}
+
+	static #orderWithPrice(orders, allMenu) {
+		const orderData = [];
+
+		orders.forEach((order) => {
+			const [name, count] = order;
+			const { price } = allMenu.find((menu) => menu.name === name);
+			orderData.push({
+				name,
+				count: Number(count),
+				totalPrice: count * price,
+			});
+		});
+
+		return orderData;
+	}
+
+	static #getOrderDataWithPrice(orders, menuBoard) {
+		const allMenu = menuBoard.getAllMenuList();
+
+		return this.#orderWithPrice(orders, allMenu);
+	}
+
+	get totalPrice() {
+		return this.#order.reduce(
+			(accumulator, currentValueders) => accumulator + currentValueders.totalPrice,
+			0,
+		);
+	}
+
+	get order() {
+		return this.#order;
 	}
 }
 
